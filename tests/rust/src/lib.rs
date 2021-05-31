546a31c534beacc38848e3afd392893f2b358062
tchataigner <tom.chataigner@yahoo.fr>
2021-05-31 08:00:52 +0200

    :white_check_mark: Added a sample transformation file in rust

---
+++ tests/rust/src/lib.rs
@@ -0,0 +1,17 @@
+use holium_wasm::{get_payload, set_payload};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct MyStruct {
+    string: String,
+    uint: u32,
+}
+
+#[no_mangle]
+pub extern "C" fn main() {
+    let payload = MyStruct {
+        string: String::from("hello"),
+        uint: 0,
+    };
+    let res = holium_wasm::set_payload("testA", &payload).unwrap();
+}
