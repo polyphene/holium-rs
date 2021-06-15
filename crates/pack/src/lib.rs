da7ce0f131cba39c8b26ddc3e39049975459d517
PhilippeMts <phi.metais@gmail.com>
2021-06-15 19:14:58 +0200

    :white_check_mark: a pack can be validated

--- crates/pack/src/lib.rs
+++ crates/pack/src/lib.rs
@@ -1,3 +1,13 @@
 extern crate rmp;
 
 pub type HoliumPack = Vec<u8>;
+
+pub trait Validatable {
+    fn validate(&self) -> bool;
+}
+
+impl Validatable for HoliumPack {
+    fn validate(&self) -> bool {
+        true
+    }
+}
--- crates/pack/tests/lib.rs
+++ crates/pack/tests/lib.rs
@@ -1,7 +1,13 @@
-extern crate holium_pack as pack;
-use pack::{HoliumPack};
+extern crate holium_pack;
+use holium_pack::{HoliumPack, Validatable};
 
 #[test]
 fn pack_is_bytes() {
     let _: HoliumPack = vec![0xc3, 0xc3];
+}
+
+#[test]
+fn pack_can_be_validated() {
+    let pack: HoliumPack = vec![0xc3, 0xc3];
+    assert!(pack.validate())
 }
