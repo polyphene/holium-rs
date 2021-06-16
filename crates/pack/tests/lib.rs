2d816c504aae70777200023fb843eb3b219b9723
PhilippeMts <phi.metais@gmail.com>
2021-06-16 09:26:03 +0200

    :building_construction: refactor tests on integers

--- crates/pack/tests/lib.rs
+++ crates/pack/tests/lib.rs
@@ -41,25 +41,57 @@ fn true_bool_msg_pack_is_valid_holium_pack() {
 }
 
 #[test]
-fn positive_fixint_msg_pack_is_valid_holium_pack() {
+fn int_0_is_valid_holium_pack() {
     let mut pack: HoliumPack = Vec::new();
-    rmp::encode::write_pfix(&mut pack, 0).unwrap();
+    rmp::encode::write_sint(&mut pack, 0).unwrap();
     assert!(pack.validate());
-    assert_eq!(1, pack.len());
-    pack.clear();
-    rmp::encode::write_pfix(&mut pack, 127).unwrap();
-    assert!(pack.validate());
+}
+
+#[test]
+fn int_0_should_take_1_byte() {
+    let mut pack: HoliumPack = Vec::new();
+    rmp::encode::write_sint(&mut pack, 0).unwrap();
     assert_eq!(1, pack.len());
 }
 
 #[test]
-fn negative_fixint_msg_pack_is_valid_holium_pack() {
+fn int_127_is_valid_holium_pack() {
     let mut pack: HoliumPack = Vec::new();
-    rmp::encode::write_nfix(&mut pack, -1).unwrap();
+    rmp::encode::write_sint(&mut pack, 127).unwrap();
     assert!(pack.validate());
+}
+
+#[test]
+fn int_127_should_take_1_byte() {
+    let mut pack: HoliumPack = Vec::new();
+    rmp::encode::write_sint(&mut pack, 127).unwrap();
     assert_eq!(1, pack.len());
-    pack.clear();
-    rmp::encode::write_nfix(&mut pack, -32).unwrap();
+}
+
+#[test]
+fn int_minus_1_is_valid_holium_pack() {
+    let mut pack: HoliumPack = Vec::new();
+    rmp::encode::write_sint(&mut pack, -1).unwrap();
     assert!(pack.validate());
+}
+
+#[test]
+fn int_minus_1_should_take_1_byte() {
+    let mut pack: HoliumPack = Vec::new();
+    rmp::encode::write_sint(&mut pack, -1).unwrap();
+    assert_eq!(1, pack.len());
+}
+
+#[test]
+fn int_minus_32_is_valid_holium_pack() {
+    let mut pack: HoliumPack = Vec::new();
+    rmp::encode::write_sint(&mut pack, -32).unwrap();
+    assert!(pack.validate());
+}
+
+#[test]
+fn int_minus_32_should_take_1_byte() {
+    let mut pack: HoliumPack = Vec::new();
+    rmp::encode::write_sint(&mut pack, -32).unwrap();
     assert_eq!(1, pack.len());
 }
