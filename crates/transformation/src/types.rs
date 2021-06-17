1a24c0a6f45f13c11cf545fce89fcfa5481f0a38
tchataigner <tom.chataigner@yahoo.fr>
2021-06-17 10:55:04 +0200

    :memo: Added a bit of doc

--- crates/transformation/src/types.rs
+++ crates/transformation/src/types.rs
@@ -1,17 +1,21 @@
+
+/// A `Package` structure is a Rust representation of a package in the Holium Framework. A `Package`
+/// is mainly composed of a wasm bytecode that contains `Transformation`.
+#[derive(Debug)]
+pub struct Package {
+
+}
+
+impl Package {
+
+}
+
+/// A `Transformation` is a wasm function that can be accessed in a `Package` bytecode.
 #[derive(Debug)]
 pub struct Transformation {
 
 }
 
 impl Transformation {
-
-}
-
-#[derive(Debug)]
-pub struct Handle {
-
-}
-
-impl Handle {
 
 }
