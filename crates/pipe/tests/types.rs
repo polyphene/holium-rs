fb5e23cd286316a244ddaf089ed5cd3aae9bedd2
tchataigner <tom.chataigner@yahoo.fr>
2021-06-28 09:18:11 +0200

    :white_check_mark: Updated tests to improve code

--- crates/pipe/tests/types.rs
+++ crates/pipe/tests/types.rs
@@ -3,15 +3,52 @@ use holium_pipe::types::Connector;
 
 #[test]
 fn test_new_connector() {
-    let cid = String::from("cid");
-    let outputs: Vec<String> = vec![];
-    let inputs: Vec<String> = vec![];
+    /*************************************************************
+     * Different length in io
+     *************************************************************/
 
-    let connector: Connector = Connector::new(cid.clone(), outputs.clone(), inputs.clone());
+    let cid = String::from("cid");
+    let outputs_index: Vec<String> = vec![String::from("0")];
+    let inputs_index: Vec<String> = vec![];
+
+    let result_new_connector =
+        Connector::new(cid.clone(), outputs_index.clone(), inputs_index.clone());
+
+    let supposed_error = PipeError::InvalidMappingError;
+
+    assert_eq!(true, result_new_connector.is_err());
+    assert_eq!(supposed_error, result_new_connector.err().unwrap());
+
+    /*************************************************************
+     * Wring index in io
+     *************************************************************/
+
+    let cid = String::from("cid");
+    let outputs_index: Vec<String> = vec![String::from("0")];
+    let inputs_index: Vec<String> = vec![String::from("a")];
+
+    let result_new_connector =
+        Connector::new(cid.clone(), outputs_index.clone(), inputs_index.clone());
+
+    let supposed_error = PipeError::InvalidMappingFormat;
+
+    assert_eq!(true, result_new_connector.is_err());
+    assert_eq!(supposed_error, result_new_connector.err().unwrap());
+
+    /*************************************************************
+     * Good behaviour test
+     *************************************************************/
+
+    let cid = String::from("cid");
+    let outputs_index: Vec<String> = vec![];
+    let inputs_index: Vec<String> = vec![];
+
+    let connector: Connector =
+        Connector::new(cid.clone(), outputs_index.clone(), inputs_index.clone()).unwrap();
 
     assert_eq!(cid, connector.cid);
-    assert_eq!(outputs, connector.outputs);
-    assert_eq!(inputs, connector.inputs);
+    assert_eq!(outputs_index, connector.outputs_index);
+    assert_eq!(inputs_index, connector.inputs_index);
 }
 
 #[test]
@@ -20,7 +57,7 @@ fn test_parse_from_connector() {
      * Missing part in string
      *************************************************************/
 
-    let missing_part_string = String::from("iamacid 0,1,2");
+    let missing_part_string = "iamacid 0,1,2";
 
     let parsed_connector = Connector::parse(missing_part_string);
 
@@ -34,7 +71,7 @@ fn test_parse_from_connector() {
      * Wrong mapping format
      *************************************************************/
 
-    let wrong_mapping_format_string = String::from("iamacid 0,1,2 0,1,a");
+    let wrong_mapping_format_string = "iamacid 0,1,2 0,1,a";
 
     let parsed_connector = Connector::parse(wrong_mapping_format_string);
 
@@ -48,7 +85,7 @@ fn test_parse_from_connector() {
      * Different number of outputs / inputs
      *************************************************************/
 
-    let different_io_nbr_string = String::from("iamacid 0,1,2 0,1");
+    let different_io_nbr_string = "iamacid 0,1,2 0,1";
 
     let parsed_connector = Connector::parse(different_io_nbr_string);
 
@@ -62,7 +99,7 @@ fn test_parse_from_connector() {
      * Wrong range in mapping
      *************************************************************/
 
-    let wrong_range_string = String::from("iamacid 0,1,2.0.0.0-2.1.0.4 0,1,2-6");
+    let wrong_range_string = "iamacid 0,1,2.0.0.0-2.1.0.4 0,1,2-6";
 
     let parsed_connector = Connector::parse(wrong_range_string);
 
@@ -76,20 +113,20 @@ fn test_parse_from_connector() {
      * Good behaviour test
      *************************************************************/
 
-    let good_connector_string = String::from("iamacid 0,1,2.0.0.0-2.0.0.4 0,1,2-6");
+    let good_connector_string = "iamacid 0,1,2.0.0.0-2.0.0.4 0,1,2-6";
     let cid = String::from("iamacid");
-    let outputs: Vec<String> = vec![
+    let outputs_index: Vec<String> = vec![
         "0", "1", "2.0.0.0", "2.0.0.1", "2.0.0.2", "2.0.0.3", "2.0.0.4",
     ]
     .into_iter()
     .map(|i| String::from(i))
     .collect();
-    let inputs: Vec<String> = vec!["0", "1", "2", "3", "4", "5", "6"]
+    let inputs_index: Vec<String> = vec!["0", "1", "2", "3", "4", "5", "6"]
         .into_iter()
         .map(|i| String::from(i))
         .collect();
 
-    let should_be_connector = Connector::new(cid, outputs, inputs);
+    let should_be_connector = Connector::new(cid, outputs_index, inputs_index).unwrap();
     let parsed_connector = Connector::parse(good_connector_string).unwrap();
 
     assert_eq!(should_be_connector, parsed_connector);
@@ -98,33 +135,10 @@ fn test_parse_from_connector() {
 #[test]
 fn test_serialize_connector() {
     /*************************************************************
-     * Different length of outputs / inputs
-     *************************************************************/
-    let cid = String::from("iamacid");
-    let outputs: Vec<String> = vec!["0", "2.0.0.0", "2.0.0.1", "2.0.0.2", "2.0.0.3", "2.0.0.4"]
-        .into_iter()
-        .map(|i| String::from(i))
-        .collect();
-    let inputs: Vec<String> = vec!["0", "1", "2", "3", "4", "5", "6"]
-        .into_iter()
-        .map(|i| String::from(i))
-        .collect();
-
-    let mut different_nbr_io_connector = Connector::new(cid, outputs, inputs);
-
-    let serialized_connector = different_nbr_io_connector.serialize();
-
-    assert_eq!(true, serialized_connector.is_err());
-    assert_eq!(
-        PipeError::SerializationError,
-        serialized_connector.err().unwrap()
-    );
-
-    /*************************************************************
      * Good behaviour test
      *************************************************************/
 
-    let good_connector_string = String::from("iamacid 0-1,2.0.0.0-2.0.0.4 0,3-6,8-9");
+    let good_connector_string = "iamacid 0-1,2.0.0.0-2.0.0.4 0,3-6,8-9";
 
     let mut parsed_connector = Connector::parse(good_connector_string.clone()).unwrap();
 
@@ -138,14 +152,14 @@ fn test_add_mapping_connector() {
     let output_index = String::from("0");
     let input_index = String::from("2.4");
 
-    let connector_string = String::from("iamacid 1,2.0.0.0-2.0.0.4,3 0,3-6,8-9");
+    let connector_string = "iamacid 1,2.0.0.0-2.0.0.4,3 0,3-6,8-9";
 
     let mut parsed_connector = Connector::parse(connector_string).unwrap();
 
     parsed_connector.add_mapping(output_index, input_index);
 
     let should_be_connector =
-        Connector::parse(String::from("iamacid 0,1,2.0.0.0-2.0.0.4,3 2.4,0,3-6,8-9")).unwrap();
+        Connector::parse("iamacid 0,1,2.0.0.0-2.0.0.4,3 2.4,0,3-6,8-9").unwrap();
 
     assert_eq!(should_be_connector, parsed_connector);
 }
