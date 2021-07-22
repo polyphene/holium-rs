74e929915cb03039213c3322e41e21b3a087afcb
PhilippeMts <phi.metais@gmail.com>
2021-07-22 16:21:28 +0200

    ✨ Introduce shadow merging of configuration objects

--- cli/src/config/mod.rs
+++ cli/src/config/mod.rs
@@ -2,7 +2,7 @@
 
 mod validation;
 
-use crate::config::validation::ConfigTemplate;
+use crate::config::validation::{ConfigTemplate, ShadowMerge};
 use crate::utils;
 use anyhow::Result;
 use dirs::config_dir;
@@ -41,7 +41,7 @@ struct MergedConfig {
     /// Configuration resulting from the merging process
     config: validation::ConfigTemplate,
     /// Individual fragments making for the merged configuration
-    fragments: [Config],
+    fragments: Vec<Config>,
 }
 
 /// Base structure for manipulation of a configuration file
@@ -72,6 +72,37 @@ impl Config {
     }
 }
 
+impl MergedConfig {
+    /// Takes an optional holium directory path and merges all existing configuration objects in
+    /// shadowing order to output a unified merged configuration.
+    pub fn new(holium_dir: Option<PathBuf>) -> Result<MergedConfig> {
+        // Initialize a default configuration and list of fragments
+        let mut config = ConfigTemplate::standard();
+        let mut fragments: Vec<Config> = Vec::with_capacity(LEVELS.len());
+
+        // Loop over all existing levels in shadowing order, get related configurations and merge them
+        for level in LEVELS.iter() {
+            // if no directory is provided, do not treat local levels
+            // otherwise, get fragment of configuration
+            let dir = match level {
+                &ConfigLevel::Global => None,
+                &ConfigLevel::Repo | &ConfigLevel::Local => {
+                    if holium_dir.is_none() {
+                        break;
+                    }
+                    holium_dir.clone()
+                }
+            };
+            let fragment = Config::new(level.clone(), dir)?;
+            // merge and push
+            config.shadow_merge(fragment.config.clone());
+            fragments.push(fragment);
+        }
+
+        Ok(MergedConfig { config, fragments })
+    }
+}
+
 /// Gets the path of a config file for a specific level and, if relevant, a holium project directory path
 fn get_config_file_path(level: &ConfigLevel, holium_dir: Option<PathBuf>) -> Result<PathBuf> {
     Ok(match level {
@@ -94,6 +125,7 @@ fn get_global_holium_dir() -> Result<PathBuf> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use assert_fs::prelude::*;
 
     #[test]
     fn can_build_config_object() {
@@ -107,6 +139,62 @@ mod tests {
 
         assert_eq!(config.level, original_level);
         assert_eq!(config.path, original_path.join("config"));
-        assert!(config.config.core.is_none());
+        assert_eq!(config.config.core.no_scm, None);
+    }
+
+    #[test]
+    fn can_build_default_merged_config_object() {
+        // Work on an empty directory
+        let temp_dir = assert_fs::TempDir::new().unwrap();
+
+        // Build a merged config with no additional configuration
+        let original_path = temp_dir.to_path_buf();
+        let config = MergedConfig::new(Some(original_path)).unwrap();
+
+        assert_eq!(config.fragments.len(), 3);
+        assert_eq!(config.fragments[0].level, ConfigLevel::Global);
+        assert_eq!(config.fragments[1].level, ConfigLevel::Repo);
+        assert_eq!(config.fragments[2].level, ConfigLevel::Local);
+        assert_eq!(config.config.core.no_scm, ConfigTemplate::standard().core.no_scm);
+    }
+
+    #[test]
+    fn can_build_merged_config_out_of_an_holium_repo() {
+        // Build a merged config out of an holium repository
+        let config = MergedConfig::new(None).unwrap();
+
+        assert_eq!(config.fragments.len(), 1);
+        assert_eq!(config.fragments[0].level, ConfigLevel::Global);
+        assert_eq!(config.config.core.no_scm, ConfigTemplate::standard().core.no_scm);
+    }
+
+    #[test]
+    fn can_merge_configuration_0() {
+        // Prepare two valid configuration
+        let repo_conf_str = r#"
+            [core]
+                no_scm = true
+        "#;
+        let local_conf_str = r#"
+            [core]
+                no_scm = false
+        "#;
+
+        // Create configuration files
+        let temp_dir = assert_fs::TempDir::new().unwrap();
+        let repo_conf_path = temp_dir.child("config");
+        let local_conf_path = temp_dir.child("config.local");
+        repo_conf_path.write_str(repo_conf_str).unwrap();
+        local_conf_path.write_str(local_conf_str).unwrap();
+
+        // Build a merged config for this project
+        let original_path = temp_dir.to_path_buf();
+        let config = MergedConfig::new(Some(original_path)).unwrap();
+
+        // Test
+        assert_eq!(config.fragments.len(), 3);
+        assert_eq!(config.config.core.no_scm, Some(false));
+        assert!(config.config.core.no_dvc.is_some());
+        assert_eq!(config.config.core.no_dvc, ConfigTemplate::standard().core.no_dvc);
     }
 }
--- cli/src/config/validation.rs
+++ cli/src/config/validation.rs
@@ -6,21 +6,62 @@ use std::fs::File;
 use std::io::Read;
 use std::path::PathBuf;
 
-#[derive(Serialize, Deserialize, Default)]
+#[derive(Serialize, Deserialize, Clone, Default)]
 /// Configuration structure used for data validation.
 pub struct ConfigTemplate {
-    pub(crate) core: Option<CoreTemplate>,
+    pub(crate) core: CoreTemplate,
 }
 
-#[derive(Serialize, Deserialize, Default)]
+#[derive(Serialize, Deserialize, Clone, Default)]
 pub(crate) struct CoreTemplate {
     pub(crate) no_scm: Option<bool>,
     pub(crate) no_dvc: Option<bool>,
 }
 
 impl ConfigTemplate {
+    /// The standard configuration holds default non-empty values for all configuration fields.
+    ///
+    /// Two different structures could be used here instead of one, implementing the `Into` trait.
+    pub fn standard() -> Self {
+        ConfigTemplate {
+            core: CoreTemplate {
+                no_scm: Some(false),
+                no_dvc: Some(false),
+            },
+        }
+    }
+}
+
+/// Provides the `ShadowMerge` trait for objects that can be merged in shadowing order.
+/// A derive macro could easily generate implementation code for structures.
+pub trait ShadowMerge {
+    fn shadow_merge(&mut self, other: Self);
+}
+
+impl ShadowMerge for ConfigTemplate {
+    fn shadow_merge(&mut self, other: Self) {
+        self.core.shadow_merge(other.core);
+    }
+}
+
+impl ShadowMerge for CoreTemplate {
+    fn shadow_merge(&mut self, other: Self) {
+        self.no_scm.shadow_merge(other.no_scm);
+        self.no_dvc.shadow_merge(other.no_dvc);
+    }
+}
+
+impl<T> ShadowMerge for Option<T> {
+    fn shadow_merge(&mut self, other: Self) {
+        if other.is_some() {
+            *self = other;
+        }
+    }
+}
+
+impl ConfigTemplate {
     /// Parses a configuration file defined by its path.
-    /// Returns the default configuration if the path does not exists.}
+    /// Returns the default sparse configuration if the path does not exists.
     pub fn from_config_file(path: &PathBuf) -> Result<ConfigTemplate> {
         // If the file does not exist, return an empty configuration
         if !path.exists() || !path.is_file() {
@@ -54,7 +95,7 @@ mod tests {
         // Try to parse it
         let config = ConfigTemplate::from_config_file(&conf_path.to_path_buf()).unwrap();
 
-        assert_eq!(config.core.unwrap().no_scm.unwrap(), true);
+        assert_eq!(config.core.no_scm.unwrap(), true);
     }
 
     #[test]
@@ -62,7 +103,66 @@ mod tests {
         // Try to parse non existent configuration file
         let path = PathBuf::new();
         let config = ConfigTemplate::from_config_file(&path).unwrap();
+        // Default sparse configuration should thus be returned
+        assert!(config.core.no_scm.is_none());
+    }
 
-        assert!(config.core.is_none());
+
+    /// Utility function used to scaffold tests on the `core.no_scp` configuration field.
+    fn test_merge_config(shadowed: Option<bool>, shadowing: Option<bool>, expected: Option<bool>) {
+        // Prepare original configurations
+        let mut config = ConfigTemplate::default();
+        config.core.no_scm = shadowed;
+        let mut shadowing_config = ConfigTemplate::default();
+        shadowing_config.core.no_scm = shadowing;
+        // Merge configurations
+        config.shadow_merge(shadowing_config);
+        // Test
+        assert_eq!(config.core.no_scm, expected);
+    }
+
+    #[test]
+    fn can_merge_configs_none_none() {
+        test_merge_config(
+            None,
+            None,
+            None,
+        )
+    }
+
+    #[test]
+    fn can_merge_configs_none_true() {
+        test_merge_config(
+            None,
+            Some(true),
+            Some(true),
+        )
+    }
+
+    #[test]
+    fn can_merge_configs_true_none() {
+        test_merge_config(
+            Some(true),
+            None,
+            Some(true),
+        )
+    }
+
+    #[test]
+    fn can_merge_configs_true_false() {
+        test_merge_config(
+            Some(true),
+            Some(false),
+            Some(false),
+        )
+    }
+
+    #[test]
+    fn can_merge_configs_false_true() {
+        test_merge_config(
+            Some(false),
+            Some(true),
+            Some(true),
+        )
     }
 }
