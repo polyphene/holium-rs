2469608dd8d3e5da605500622dfe3bdb92a1fe9f
tchataigner <tom.chataigner@yahoo.fr>
2021-10-12 17:42:55 +0200

    :recycle: Updated data mod for visibility and access to util functions

--- crates/cli/src/data/import/mod.rs
+++ crates/cli/src/data/import/mod.rs
@@ -2,20 +2,20 @@ use std::env;
 use std::path::Path;
 
 use anyhow::{Context, Result};
-use clap::{App, Arg, arg_enum, ArgMatches, SubCommand, value_t};
+use clap::{arg_enum, value_t, App, Arg, ArgMatches, SubCommand};
 
 use holium::data::data_tree::Node as DataTreeNode;
 use holium::data::linked_data_tree::Node as LinkedDataTreeNode;
 
 use crate::data::DataError;
-use crate::utils::PROJECT_DIR;
 use crate::utils::storage::RepoStorage;
+use crate::utils::PROJECT_DIR;
 
 mod importer;
 
-
+// Pub enum here as clap does not handle pub(crate)
 arg_enum! {
-    enum ImportType {
+    pub enum ImportType {
         cbor,
         json,
         bin,
@@ -31,7 +31,7 @@ pub(crate) fn import_cmd<'a, 'b>() -> App<'a, 'b> {
             Arg::with_name("path")
                 .help("The path of the file to import")
                 .index(1)
-                .required(true)
+                .required(true),
         )
         .arg(
             Arg::with_name("type")
@@ -40,41 +40,56 @@ pub(crate) fn import_cmd<'a, 'b>() -> App<'a, 'b> {
                 .short("t")
                 .long("type")
                 .possible_values(&ImportType::variants())
-                .required(true)
+                .required(true),
         )
 }
 
 /// `data` `import` command handler
 pub(crate) fn handle_import_cmd(matches: &ArgMatches) -> Result<()> {
-    // Initialize context handler
-    let repo_storage = RepoStorage::from_cur_dir()?;
     // Get the path of the file to be imported
     let path = Path::new(
         matches
             .value_of("path")
-            .context(DataError::FailedToOpenImportFile)?
+            .context(DataError::FailedToOpenImportFile)?,
     );
     // Get the type of the file from CLI argument
-    let t = value_t!(matches.value_of("type"), ImportType)
-        .context(DataError::InvalidImportFileTypeOptionValue(
-            matches.value_of("type").unwrap_or("").to_string())
-        )?;
+    let t = matches_to_import_ty(matches)?;
+    // Generate data node
+    let data_tree = file_to_data_tree(path, t)?;
+    // Build & store linked data tree
+    let root_object_cid = store_data_tree(data_tree)?;
+    // Print object cid
+    println!("{}", &root_object_cid);
+    // return
+    Ok(())
+}
+
+pub(crate) fn matches_to_import_ty(matches: &ArgMatches) -> Result<ImportType> {
+    Ok(value_t!(matches.value_of("type"), ImportType).context(
+        DataError::InvalidImportFileTypeOptionValue(
+            matches.value_of("type").unwrap_or("").to_string(),
+        ),
+    )?)
+}
+
+pub(crate) fn file_to_data_tree(file_path: &Path, import_type: ImportType) -> Result<DataTreeNode> {
     // Parse the file content into a data tree
-    let importer: Box<dyn importer::Importer> = match t {
+    let importer: Box<dyn importer::Importer> = match import_type {
         ImportType::cbor => Box::new(importer::Cbor()),
         ImportType::json => Box::new(importer::Json()),
         ImportType::bin => Box::new(importer::Bin()),
         ImportType::csv => Box::new(importer::Csv()),
     };
-    let importable_value = importer.import(path)?;
+    let importable_value = importer.import(file_path)?;
     let cbor_value = importable_value.to_cbor();
-    let data_tree = DataTreeNode::new(cbor_value);
+    Ok(DataTreeNode::new(cbor_value))
+}
+
+pub(crate) fn store_data_tree(data_tree: DataTreeNode) -> Result<String> {
+    // Initialize context handler
+    let repo_storage = RepoStorage::from_cur_dir()?;
     // Build linked data tree
     let linked_data_tree = LinkedDataTreeNode::from_data_tree(data_tree)?;
     // Store linked data tree
-    let root_object_cid = repo_storage.write_data_tree(&linked_data_tree)?;
-    // Print object cid
-    println!("{}", &root_object_cid);
-    // return
-    Ok(())
+    Ok(repo_storage.write_data_tree(&linked_data_tree)?)
 }
