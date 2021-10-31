de5cae9bb046d80c651ffdb33bfca03d808049f7
tchataigner <tom.chataigner@yahoo.fr>
2021-11-01 00:53:31 +0100

    :white_check_mark: Added tests for helper selector

--- crates/cli/src/utils/local/helpers/selector/mod.rs
+++ crates/cli/src/utils/local/helpers/selector/mod.rs
@@ -17,6 +17,8 @@ lazy_static::lazy_static! {
 
 #[derive(thiserror::Error, Debug)]
 enum Error {
+    #[error("invalid string can not be passed to json")]
+    StringNotParsableToJSON,
     #[error("invalid holium selector")]
     InvalidHoliumSelector,
 }
@@ -24,9 +26,34 @@ enum Error {
 /// Validate a Holium selector JSON instance against the reference JSON Schema.
 pub fn validate_selector(literal: &str) -> Result<()> {
     // parse the instance literal into serde_json::Value
-    let instance: Value = serde_json::from_str(literal)?;
+    let instance: Value = serde_json::from_str(literal).context(Error::StringNotParsableToJSON)?;
     // validate it against Holium selector schema
     HOLIUM_SELECTOR_SCHEMA.validate(&instance)
         .ok()
         .context(Error::InvalidHoliumSelector)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn cannot_validate_non_json_string() {
+        let non_json = "i am not a json";
+
+        let res = validate_selector(non_json);
+
+        assert!(res.is_err());
+        assert!(res.err().unwrap().to_string().contains("invalid string can not be passed to json"));
+    }
+
+    #[test]
+    fn cannot_validate_non_valid_json_object() {
+        let non_json = "{\"non\":\"valid\"}";
+
+        let res = validate_selector(non_json);
+
+        assert!(res.is_err());
+        assert!(res.err().unwrap().to_string().contains("invalid holium selector"));
+    }
 }
