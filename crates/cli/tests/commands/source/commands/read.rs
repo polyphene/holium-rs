c1fb21749605c6aa84b635a20ee3f32127d545de
tchataigner <tom.chataigner@yahoo.fr>
2021-11-07 19:28:42 +0100

    :ok_hand: Removed can_read_source test as it is redundent with can_create_source

--- crates/cli/tests/commands/source/commands/read.rs
+++ crates/cli/tests/commands/source/commands/read.rs
@@ -1,7 +1,7 @@
-use assert_cmd::Command;
-use predicates::prelude::predicate;
 use crate::helpers::repo::setup_repo;
 use crate::helpers::source::{build_source_read_cmd, setup_repo_with_source, SOURCE_NAME};
+use assert_cmd::Command;
+use predicates::prelude::predicate;
 
 #[test]
 fn help_available() {
@@ -10,7 +10,6 @@ fn help_available() {
     // Check success
     assert.success();
 }
-
 
 #[test]
 fn cannot_read_source_without_name() {
@@ -39,19 +38,4 @@ fn cannot_read_non_existent_source() {
     assert
         .failure()
         .stderr(predicate::str::contains("missing object for key"));
-}
-
-#[test]
-fn can_read_source() {
-    // initialize a repository
-    let repo = setup_repo_with_source();
-    let repo_path = repo.path();
-
-    // try to read source
-    let assert = build_source_read_cmd(repo_path, SOURCE_NAME);
-    // check output
-    assert
-        .success()
-        .stdout(predicate::str::contains(SOURCE_NAME))
-        .stdout(predicate::str::contains("\"type\": \"string\""));
 }
