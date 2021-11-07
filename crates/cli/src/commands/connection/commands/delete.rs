fcc822bd84b4e8f1dd560fb4a3e1d75d2b8c7242
tchataigner <tom.chataigner@yahoo.fr>
2021-11-07 10:27:09 +0100

    :ok_hand: Removed unit testing from print functions

--- crates/cli/src/commands/connection/commands/delete.rs
+++ crates/cli/src/commands/connection/commands/delete.rs
@@ -1,20 +1,20 @@
-use anyhow::{Result, Context};
-use clap::{App, SubCommand, Arg, ArgMatches};
+use crate::utils::errors::Error::{
+    DbOperationFailed, MissingRequiredArgument, NoObjectForGivenKey,
+};
 use crate::utils::local::context::LocalContext;
-use crate::utils::errors::Error::{MissingRequiredArgument, DbOperationFailed, NoObjectForGivenKey};
-use console::style;
 use crate::utils::local::helpers::prints::commands_outputs::print_delete_success;
+use anyhow::{Context, Result};
+use clap::{App, Arg, ArgMatches, SubCommand};
+use console::style;
 
 /// command
 pub(crate) fn cmd<'a, 'b>() -> App<'a, 'b> {
     SubCommand::with_name("delete")
         .about("Delete a connection")
-        .args(&[
-            Arg::with_name("id")
-                .help("ID of the connection")
-                .required(true)
-                .value_name("ID"),
-        ])
+        .args(&[Arg::with_name("id")
+            .help("ID of the connection")
+            .required(true)
+            .value_name("ID")])
 }
 
 /// handler
@@ -22,15 +22,18 @@ pub(crate) fn handle_cmd(matches: &ArgMatches) -> Result<()> {
     // create local context
     let local_context = LocalContext::new()?;
     // get argument values
-    let id = matches.value_of("id")
+    let id = matches
+        .value_of("id")
         .context(MissingRequiredArgument("id".to_string()))?;
     // delete object from local database
-    let old_value = local_context.connections.remove(id)
+    let old_value = local_context
+        .connections
+        .remove(id)
         .context(DbOperationFailed)?;
     if old_value.is_none() {
         return Err(NoObjectForGivenKey(id.to_string()).into());
     }
     // print
-    print_delete_success(&mut std::io::stdout(), id)?;
+    print_delete_success(id);
     Ok(())
 }
--- crates/cli/src/utils/local/helpers/prints/commands_outputs/mod.rs
+++ crates/cli/src/utils/local/helpers/prints/commands_outputs/mod.rs
@@ -5,71 +5,49 @@ use console::style;
 use std::io::Write;
 use thiserror::Error;
 
-#[derive(Debug, Error)]
-enum Error {
-    #[error("error while writing success message")]
-    FailedToWriteSuccessMessage,
-    #[error("error while writing update message")]
-    FailedToWriteUpdateMessage,
-    #[error("error while writing delete message")]
-    FailedToWriteDeleteMessage,
-    #[error("error while writing health success message")]
-    FailedToWriteHealthSuccessMessage,
-    #[error("error while writing project export success message")]
-    FailedToWriteProjectExportSuccessMessage,
-}
 /*
 Success messages
 */
 
 /// Print CREATE method success message.
-pub fn print_create_success(writter: &mut Write, key: &str) -> Result<()> {
-    writeln!(
-        writter,
+pub fn print_create_success(key: &str) {
+    println!(
         "{}",
         style(format!("new object created: {}", style(key).bold())).green()
     )
-    .context(Error::FailedToWriteSuccessMessage)
 }
 
 /// Print UPDATE method success message.
-pub fn print_update_success(writter: &mut Write, key: &str) -> Result<()> {
-    writeln!(
-        writter,
+pub fn print_update_success(key: &str) {
+    println!(
         "{}",
         style(format!("object updated: {}", style(key).bold())).green()
     )
-    .context(Error::FailedToWriteUpdateMessage)
 }
 
 /// Print DELETE method success message.
-pub fn print_delete_success(writter: &mut Write, key: &str) -> Result<()> {
-    writeln!(
-        writter,
+pub fn print_delete_success(key: &str) {
+    println!(
         "{}",
         style(format!("object deleted: {}", style(key).bold())).green()
     )
-    .context(Error::FailedToWriteDeleteMessage)
 }
 
 /// Print success message for methods checking the health of the transformation pipeline currently
 /// in the local area.
-pub fn print_pipeline_health_success(writter: &mut Write) -> Result<()> {
-    writeln!(
-        writter,
+pub fn print_pipeline_health_success() {
+    println!(
         "{}",
         style("current project holds a healthy transformation pipeline").green()
     )
-    .context(Error::FailedToWriteHealthSuccessMessage)
 }
 
 /// Print project EXPORT success message.
-pub fn print_project_export_success(writter: &mut Write, cid: &Cid) -> Result<()> {
+pub fn print_project_export_success(cid: &Cid) {
     let cid_str = cid
         .to_string_of_base(DEFAULT_MULTIBASE)
         .unwrap_or("".to_string());
-    writeln!(
-        writter,
+    println!(
         "{}",
         style(format!(
             "project exported with pipeline cid: {}",
@@ -77,72 +55,4 @@ pub fn print_project_export_success(writter: &mut Write, cid: &Cid) -> Result<()
         ))
         .green()
     )
-    .context(Error::FailedToWriteProjectExportSuccessMessage)
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-    use std::str::FromStr;
-
-    #[test]
-    fn can_have_formatted_msg_for_create_success() {
-        let mut stdout = Vec::new();
-        let key = "my_key";
-        let awaited_msg = format!("new object created: {}\n", &key);
-
-        // pass fake stdout when calling when testing
-        print_create_success(&mut stdout, key).unwrap();
-
-        assert_eq!(awaited_msg.as_bytes(), stdout);
-    }
-
-    #[test]
-    fn can_have_formatted_msg_for_update_success() {
-        let mut stdout = Vec::new();
-        let key = "my_key";
-        let awaited_msg = format!("object updated: {}\n", &key);
-
-        // pass fake stdout when calling when testing
-        print_update_success(&mut stdout, key).unwrap();
-
-        assert_eq!(awaited_msg.as_bytes(), stdout);
-    }
-
-    #[test]
-    fn can_have_formatted_msg_for_delete_success() {
-        let mut stdout = Vec::new();
-        let key = "my_key";
-        let awaited_msg = format!("object deleted: {}\n", &key);
-
-        // pass fake stdout when calling when testing
-        print_delete_success(&mut stdout, key).unwrap();
-
-        assert_eq!(awaited_msg.as_bytes(), stdout);
-    }
-
-    #[test]
-    fn can_have_formatted_msg_for_health_success() {
-        let mut stdout = Vec::new();
-        let awaited_msg = "current project holds a healthy transformation pipeline\n";
-
-        // pass fake stdout when calling when testing
-        print_pipeline_health_success(&mut stdout).unwrap();
-
-        assert_eq!(awaited_msg.as_bytes(), stdout);
-    }
-
-    #[test]
-    fn can_have_formatted_msg_for_project_export_success() {
-        let mut stdout = Vec::new();
-        let cid_str = "bafybeigdyrzt5sfp7udm7hu76uh7y26nf3efuylqabf3oclgtqy55fbzdi";
-        let cid =
-            Cid::from_str("bafybeigdyrzt5sfp7udm7hu76uh7y26nf3efuylqabf3oclgtqy55fbzdi").unwrap();
-        let awaited_msg = format!("project exported with pipeline cid: {}\n", cid_str);
-
-        // pass fake stdout when calling when testing
-        print_project_export_success(&mut stdout, &cid).unwrap();
-        println!("{:?}", String::from_utf8(stdout.clone()));
-        assert_eq!(awaited_msg.as_bytes(), stdout);
-    }
 }
